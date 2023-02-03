c0976dd1da682c749d07d7e4f140f9aace7107b4
Drin <kadriudrin@gmail.com>
2023-02-03 20:50:33 +0100

    Added patroling script

---
+++ GGJ2023/Assets/Scripts/Drin/PatrolEnemy.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class PatrolEnemy : MonoBehaviour
+{
+    private Rigidbody2D body;
+
+    private bool patroling = true, shooting = false, canShoot = false;
+
+    public Transform groundCheck, bulletHole;
+
+    public LayerMask groundMask, playerLayer;
+
+    public float groundCheckRadius;
+
+    public int dir = 1;
+
+    private void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!patroling)
+            return;
+
+        if (!Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundMask))
+        {
+            Switch();
+            body.velocity = new Vector2(0, body.velocity.y);
+        }
+
+        body.AddForce(Vector2.right * dir * moveSpeed);
+    }
+
+    private void Switch()
+    {
+        dir *= -1;
+
+        Vector3 newScl = transform.localScale;
+        newScl.x *= -1;
+
+        transform.localScale = newScl;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+}
