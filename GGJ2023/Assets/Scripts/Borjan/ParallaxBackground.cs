71b81edc31e23c7b9a7d707f2f22b4991c7415d7
Borjan Peovski <51805234+borjan2@users.noreply.github.com>
2023-02-03 21:01:33 +0100

    Added parallax script

---
+++ GGJ2023/Assets/Scripts/Borjan/ParallaxBackground.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+
+    private Transform cameraTransform;
+    private Vector3 lastCameraPosition;
+
+    [SerializeField] private float parallaxEfectMultilier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition = cameraTransform.position;
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position += deltaMovement * parallaxEfectMultilier;
+        lastCameraPosition = cameraTransform.position;
+    }
+}
